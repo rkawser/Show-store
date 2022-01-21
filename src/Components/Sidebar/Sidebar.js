9228a4494fcad32982ddfa21572226945299243a
raihan kawser <raihankawser29@gmail.com>
2022-01-21 09:42:27 -0800

    fdggfd

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -3,6 +3,12 @@ import { Button, Nav, Offcanvas } from 'react-bootstrap';
 import { AiOutlineMenuUnfold } from "react-icons/ai";
 import { Link } from 'react-router-dom';
 import './Sidebar.css'
+import { AiOutlineHome} from "react-icons/ai";
+import { FaHandPointRight } from "react-icons/fa";
+import { MdPayments } from "react-icons/md";
+import { RiHandHeartFill} from "react-icons/ri";
+import { AiOutlineLogout } from "react-icons/ai";
+
 const Sidebar = () => {
     const [show, setShow] = useState(false);
 
@@ -20,11 +26,11 @@ const Sidebar = () => {
           <Offcanvas.Title>Your Profile</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-        <Nav.Link as={Link} to="/home" className='side-text'>Home</Nav.Link>
-        <Nav.Link as={Link} to="/myorders" className='side-text'>My ORders</Nav.Link>
-        <Nav.Link as={Link} to="/payment" className='side-text'>Payment</Nav.Link>
-        <Nav.Link as={Link} to="/review" className='side-text'>Review</Nav.Link>
-        <Nav.Link as={Link} to="/home" className='side-text'>LogOut</Nav.Link>
+        <Nav.Link as={Link} to="/home" className='side-text'><AiOutlineHome/> Home</Nav.Link>
+        <Nav.Link as={Link} to="/myorders" className='side-text'><FaHandPointRight/> My ORders</Nav.Link>
+        <Nav.Link as={Link} to="/payment" className='side-text'><MdPayments/> Payment</Nav.Link>
+        <Nav.Link as={Link} to="/review" className='side-text'><RiHandHeartFill/> Review</Nav.Link>
+        <Nav.Link as={Link} to="/home" className='side-text'> <AiOutlineLogout/> LogOut</Nav.Link>
         </Offcanvas.Body>
       </Offcanvas>
     </>
