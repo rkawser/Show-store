1620822e125757fed9557beed64b39bd10d9ac9e
raihan kawser <raihankawser29@gmail.com>
2022-01-26 07:25:05 +0600

    some code add

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import './Footer.css'
 import {BsFacebook } from "react-icons/bs";
-import { AiFillTwitterCircle } from "react-icons/ai";
+import {AiFillTwitterCircle } from "react-icons/ai";
 import {GrInstagram} from "react-icons/gr";
-import { AiFillGoogleCircle} from "react-icons/ai";
+import {AiFillGoogleCircle} from "react-icons/ai";
+import {FaHeadset} from "react-icons/fa";
+import {FaAddressCard} from "react-icons/fa";
+import {FiPhoneCall } from "react-icons/fi";
+import {MdEmail } from "react-icons/md";
+
 const Footer = () => {
   return (
   <div className='footer-style'>
@@ -12,15 +17,15 @@ const Footer = () => {
         <Row>
             <Col md={3}>
                 <div className='footer-details'>
-                    <h2>SHOE STORE</h2>
-                    <h3>+ (88) 01729573536</h3>
+                    <h2><span className='footer-shoe'>SHOE</span>  <span className='footer-store'> STORE</span></h2>
+                <h4><FaHeadset className='footer-call'/>(+88)01729573536</h4>
                     <h6>We are a team of designers and developers who creates high quality multipurpose and responsive premium Shopify themes.</h6>
-                    <BsFacebook className='footeer-icon'/> <AiFillTwitterCircle/> <GrInstagram/> <AiFillGoogleCircle/>
+                    <BsFacebook className='footeer-icon'/> <AiFillTwitterCircle className='footeer-icon'/> <GrInstagram className='footeer-icon'/> <AiFillGoogleCircle className='footeer-icon'/>
                 </div>
             </Col>
 
             <Col md={3}>
-                <div>
+                <div className='footer-menu'>
                     <h6>MAIN MENU</h6>
                     <p>Home</p>
                     <p>For Men</p>
@@ -46,11 +51,11 @@ const Footer = () => {
             <Col md={3}>
                 <div className='footer-get'>
                     <h6>GET IN TOUCH</h6>
-                    <p>Address :123 Main Street, Anytown,CA </p>
+                    <p><FaAddressCard/>Address:123Main Street,Anytown,CA </p>
                     <p>12345 - Bangladesh.</p>
-                    <p>Phone :+ (88) 01729573536</p>
-                    <p>Email :raihankawser29@gmail.com</p>
-                        
+                    <p><FiPhoneCall/> Phone :+ (88) 01729573536</p>
+                    <p><MdEmail/>Email :raihankawser29@gmail.com</p>
+                    <p className='footer-payment'></p>
               </div>
             </Col>       
         </Row>
