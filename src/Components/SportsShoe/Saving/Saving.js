0cc423a084e6295f66fdbfa31718f89f1e76a686
raihan kawser <raihankawser29@gmail.com>
2022-02-10 19:50:41 +0600

    new section add

--- src/Components/SportsShoe/Saving/Saving.js
+++ src/Components/SportsShoe/Saving/Saving.js
@@ -7,6 +7,15 @@ const Saving = () => {
     <Container fluid>
    <Row className='saving-image'>
        <Col>
+       <Container>
+       <div className='saving-text'>
+         <h1 className='mt-5 tex-white'>Saving 50%</h1>
+         <h3>ALL Online Store</h3>
+         <h2 className='mt-5'>OFFER AVAILABLE ALL SHOES & PRODUCTS</h2>
+
+         <button className='saving-button'>Shop Now</button>
+       </div>
+       </Container>
        
        
        </Col>
